3eccd546139a146633bdb19c6f85270e6d70aa73
Jum-eng <jmusah007@st.ug.edu.gh>
2024-07-07 03:42:28 +0000

    DrawingForm

---
+++ DrawingForm/DrawingForm/Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace DrawingApp
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button clearButton;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+
+            // panel1
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 40);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 410);
+            this.panel1.TabIndex = 0;
+
+            // clearButton
+            this.clearButton.Dock = System.Windows.Forms.DockStyle.Top;
+            this.clearButton.Location = new System.Drawing.Point(0, 0);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(800, 40);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+
+            // Form1
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.clearButton);
+            this.Name = "Form1";
+            this.Text = "Drawing App";
+            this.ResumeLayout(false);
+        }
+    }
+}
